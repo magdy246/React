e28bc5fd5b390226e96b044395b97825c12fa49d
magdy Abo El-nour <mm246344@gmail.com>
2024-08-10 05:12:42 +0300

    update portfolio comp,

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -1,4 +1,4 @@
-import React , { useState } from "react";
+import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faStar, faPlus } from "@fortawesome/free-solid-svg-icons";
 import port1 from "./../../assets/images/port1.png";
@@ -8,8 +8,10 @@ import port3 from "./../../assets/images/port3.png";
 export default function Portfolio() {
   const [src, setSrc] = useState("");
 
-  let phoTo = (event) => {
-    let clickPhoto = event.currentTarget.getAttribute("data-src");
+  const photos = [port1, port2, port3, port1, port2, port3];
+
+  const handlePhotoClick = (event) => {
+    const clickPhoto = event.currentTarget.getAttribute("data-src");
     setSrc(clickPhoto);
   };
 
@@ -27,110 +29,26 @@ export default function Portfolio() {
             </h2>
             <div className="flex items-center justify-center">
               <div className="line sec-color mr-3 h-px w-10"></div>
-              <FontAwesomeIcon
-                className="sec-color-text text-lg"
-                icon={faStar}
-              />
+              <FontAwesomeIcon className="sec-color-text text-lg" icon={faStar} />
               <div className="line sec-color ml-3 h-px w-10"></div>
             </div>
           </div>
           <div className="flex flex-row justify-center items-center w-full">
             <div className="mb-8 px-6 flex flex-row flex-wrap">
-              <div className="lg:w-1/3 md:w-1/2 w-full px-3 mt-6">
-                <div className="relative rounded-xl overflow-hidden">
-                  <div
-                    className="lay cursor-pointer flex justify-center items-center absolute bottom-0 top-0 right-0 left-0 second-color opacity-0 hover:opacity-80 transition-opacity duration-500"
-                    data-src={port1}
-                    onClick={phoTo}
-                  >
-                    <FontAwesomeIcon
-                      className="text-white text-7xl"
-                      icon={faPlus}
-                    />
+              {photos.map((photo, index) => (
+                <div key={index} className="lg:w-1/3 md:w-1/2 w-full px-3 mt-6">
+                  <div className="relative rounded-xl overflow-hidden">
+                    <div
+                      className="lay cursor-pointer flex justify-center items-center absolute bottom-0 top-0 right-0 left-0 second-color opacity-0 hover:opacity-80 transition-opacity duration-500"
+                      data-src={photo}
+                      onClick={handlePhotoClick}
+                    >
+                      <FontAwesomeIcon className="text-white text-7xl" icon={faPlus} />
+                    </div>
+                    <img className="photo w-full cursor-pointer" src={photo} alt="port" />
                   </div>
-                  <img className="photo w-full cursor-pointer" src={port1} alt="port" />
                 </div>
-              </div>
-
-              <div className="lg:w-1/3 md:w-1/2 w-full px-3 mt-6">
-                <div className="relative rounded-xl overflow-hidden">
-                  <div
-                    className="lay cursor-pointer flex justify-center items-center absolute bottom-0 top-0 right-0 left-0 second-color opacity-0 hover:opacity-80 transition-opacity duration-500"
-                    data-src={port2}
-                    onClick={phoTo}
-                  >
-                    <FontAwesomeIcon
-                      className="text-white text-7xl"
-                      icon={faPlus}
-                    />
-                  </div>
-                  <img className="photo w-full cursor-pointer" src={port2} alt="port" />
-                </div>
-              </div>
-
-              <div className="lg:w-1/3 md:w-1/2 w-full px-3 mt-6">
-                <div className="relative rounded-xl overflow-hidden">
-                  <div
-                    className="lay cursor-pointer flex justify-center items-center absolute bottom-0 top-0 right-0 left-0 second-color opacity-0 hover:opacity-80 transition-opacity duration-500"
-                    data-src={port3}
-                    onClick={phoTo}
-                  >
-                    <FontAwesomeIcon
-                      className="text-white text-7xl"
-                      icon={faPlus}
-                    />
-                  </div>
-                  <img className="photo w-full cursor-pointer" src={port3} alt="port" />
-                </div>
-              </div>
-
-              <div className="lg:w-1/3 md:w-1/2 w-full px-3 mt-6">
-                <div className="relative rounded-xl overflow-hidden">
-                  <div
-                    className="lay cursor-pointer flex justify-center items-center absolute bottom-0 top-0 right-0 left-0 second-color opacity-0 hover:opacity-80 transition-opacity duration-500"
-                    data-src={port1}
-                    onClick={phoTo}
-                  >
-                    <FontAwesomeIcon
-                      className="text-white text-7xl"
-                      icon={faPlus}
-                    />
-                  </div>
-                  <img className="photo w-full cursor-pointer" src={port1} alt="port" />
-                </div>
-              </div>
-
-              <div className="lg:w-1/3 md:w-1/2 w-full px-3 mt-6">
-                <div className="relative rounded-xl overflow-hidden">
-                  <div
-                    className="lay cursor-pointer flex justify-center items-center absolute bottom-0 top-0 right-0 left-0 second-color opacity-0 hover:opacity-80 transition-opacity duration-500"
-                    data-src={port2}
-                    onClick={phoTo}
-                  >
-                    <FontAwesomeIcon
-                      className="text-white text-7xl"
-                      icon={faPlus}
-                    />
-                  </div>
-                  <img className="photo w-full cursor-pointer" src={port2} alt="port" />
-                </div>
-              </div>
-
-              <div className="lg:w-1/3 md:w-1/2 w-full px-3 mt-6">
-                <div className="relative rounded-xl overflow-hidden">
-                  <div
-                    className="lay cursor-pointer flex justify-center items-center absolute bottom-0 top-0 right-0 left-0 second-color opacity-0 hover:opacity-80 transition-opacity duration-500"
-                    data-src={port3}
-                    onClick={phoTo}
-                  >
-                    <FontAwesomeIcon
-                      className="text-white text-7xl"
-                      icon={faPlus}
-                    />
-                  </div>
-                  <img className="photo w-full cursor-pointer" src={port3} alt="port" />
-                </div>
-              </div>
+              ))}
             </div>
           </div>
         </div>
